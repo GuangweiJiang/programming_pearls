506883450feef4a849321f1b9d9333dd3a8feb05
Guangwei Jiang <guangwei_jiang@wistron.com>
2012-08-01 14:13:11 +0800

    first commit isort1
    Owner: Guangwei Jiang
    
    Issue: N/A
    
    Description: first commit isort1
    
    Root Cause: N/A

---
+++ ch11_sorting/isort1/isort1.cpp
@@ -0,0 +1,82 @@
+/*
+file name: isort1.cpp
+purpose: 
+	P109
+	筛选过程通过一个从右到左的循环实现，该循环使用变量j跟踪被筛选的元素。
+	只要该元素具有前驱（即j>0）且没有到达最终位置（即该元素小于它的前驱），循环就交换该元素和它的前驱。
+creator: Guangwei Jiang
+create time: 2012-08-01
+
+modify history:
+
+notes:
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define ARRAY_SIZE		50000
+
+// notes: bigrand should at least 64bits
+unsigned long long bigrand()
+{
+	return RAND_MAX*rand() + rand();
+}
+
+void swap(int *p1, int *p2)
+{
+	int tmp;
+	tmp = *p1;
+	*p1 = *p2;
+	*p2 = tmp;
+}
+
+void isort1(int *array_data, int array_size)
+{
+	int i, j;
+
+	for (i = 0; i < array_size; i++)
+	{
+		for (j = i; j > 0 && array_data[j-1] > array_data[j]; j--)
+			  swap(array_data+(j-1), array_data+j);
+	}
+}
+
+int main()
+{
+	clock_t start, finish;
+	double duration;	
+	int *array_data;
+	int i;
+
+	array_data = (int*) malloc (sizeof(int)*ARRAY_SIZE);
+	if (array_data == NULL)
+	{
+		printf("memory(array_bitmap) allocate failed\n");
+		goto _free_mem;
+	}
+	
+	printf("Generate the test data...\n");
+	srand(time(0));
+	for (i = 0; i < ARRAY_SIZE; i++)
+	{
+		array_data[i] = bigrand()%RAND_MAX;
+	}
+
+	printf("Sorting data...\n");
+	start = clock();
+	isort1(array_data, ARRAY_SIZE);
+	finish = clock();
+	duration = (double)(finish - start) / CLOCKS_PER_SEC;
+	printf("Test data number is %d\n", ARRAY_SIZE);
+	printf("the duration is %f seconds\n\n", duration);
+
+_free_mem:
+	if (array_data != NULL)
+	{
+		free(array_data);
+		array_data = NULL;
+	}
+
+	return 0;
+}
