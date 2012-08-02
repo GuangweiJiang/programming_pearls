2ce43d48e9bd385cc90ac3c22275f6f082de3f5c
Guangwei Jiang <guangwei_jiang@wistron.com>
2012-08-02 15:12:00 +0800

    first commit "qsort_api" codes.
    Owner: Guangwei Jiang
    
    Issue: N/A
    
    Description: first commit "qsort_api" codes.
    
    Root Cause: N/A

---
+++ ch11_sorting/qsort_api/qsort_api.cpp
@@ -0,0 +1,122 @@
+/*
+file name: qsort_api.cpp
+purpose: 
+	Using qsort() api to do sorting.
+creator: Guangwei Jiang
+create time: 2012-08-02
+
+modify history:
+
+notes:
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define ARRAY_SIZE		5000000
+
+// notes: bigrand should at least 64bits
+unsigned long long bigrand()
+{
+	return RAND_MAX*rand() + rand();
+}
+
+void gen_random_array(int *array_data, int array_size)
+{
+	int i;
+
+	printf("The test data are random.\n");
+
+	srand(time(0));
+	for (i = 0; i < array_size; i++)
+	{
+		array_data[i] = bigrand()%RAND_MAX;
+	}
+}
+
+void gen_equal_array(int *array_data, int array_size)
+{
+	int i, test_val;
+
+	printf("The test data are equal.\n");
+
+	srand(time(0));
+	test_val = bigrand()%RAND_MAX;
+
+	for (i = 0; i < array_size; i++)
+	{
+		array_data[i] = test_val;
+	}
+}
+
+void gen_increasing_array(int *array_data, int array_size)
+{
+	int i;
+
+	printf("The test data are increasing.\n");
+
+	for (i = 0; i < array_size; i++)
+	{
+		array_data[i] = i;
+	}
+}
+
+int compare (const void * a, const void * b)
+{
+	return ( *(int*)a - *(int*)b );
+}
+
+void print_array(int *array_data, int array_size)
+{
+	int i;
+
+	for (i = 0; i < array_size; i++)
+	{
+		printf("%12d ", array_data[i]);
+
+		if ((i%10 == 0) && (i != 0))
+			  printf("\n");
+	}
+
+	printf("\n");
+}
+
+int main()
+{
+	clock_t start, finish;
+	double duration;	
+	int *array_data;
+
+	array_data = (int*) malloc (sizeof(int)*ARRAY_SIZE);
+	if (array_data == NULL)
+	{
+		printf("memory(array_bitmap) allocate failed\n");
+		goto _free_mem;
+	}
+	
+	printf("Generate the test data...\n");
+	gen_random_array(array_data, ARRAY_SIZE);
+	//gen_equal_array(array_data, ARRAY_SIZE);
+	//gen_increasing_array(array_data, ARRAY_SIZE);
+
+	printf("Sorting data...\n");
+	start = clock();
+	qsort (array_data, ARRAY_SIZE, sizeof(int), compare);
+	finish = clock();
+	duration = (double)(finish - start) / CLOCKS_PER_SEC;
+
+	// print the data after sorting
+	//print_array(array_data, ARRAY_SIZE);
+
+	printf("Test data number is %d\n", ARRAY_SIZE);
+	printf("the duration is %f seconds\n\n", duration);
+
+_free_mem:
+	if (array_data != NULL)
+	{
+		free(array_data);
+		array_data = NULL;
+	}
+
+	return 0;
+}
---
+++ ch11_sorting/qsort_api/readme.txt
@@ -0,0 +1,62 @@
+Using API <qsort> to do sorting.
+
+notes:
+Test data is 5,000,000, using "g++ qsort_api.cpp",
+If the test data are random, it costs around 1.16 seconds;
+If the test data are equal, it costs around 0.34 seconds;
+If the test data are increasing, it costs around 0.34 seconds;
+
+**********
+function-qsort
+void qsort ( void * base, size_t num, size_t size, int ( * comparator ) ( const void *, const void * ) );
+
+Sort elements of array
+Sorts the num elements of the array pointed by base, each element size bytes long, using the comparator function to determine the order.
+The sorting algorithm used by this function compares pairs of values by calling the specified comparator function with two pointers to elements of the array.
+The function does not return any value, but modifies the content of the array pointed by base reordering its elements to the newly sorted order.
+
+Parameters
+base - Pointer to the first element of the array to be sorted.
+num - Number of elements in the array pointed by base.
+size - Size in bytes of each element in the array.
+comparator - Function that compares two elements. The function shall follow this prototype:
+
+int comparator ( const void * elem1, const void * elem2 ); 
+The function must accept two parameters that are pointers to elements, type-casted as void*. 
+These parameters should be cast back to some data type and be compared.
+The return value of this function should represent whether elem1 is considered less than, equal to, or greater than elem2 by returning, respectively, a negative value, zero or a positive value.
+
+Return Value
+(none) 
+
+**********
+Example
+/* qsort example */
+#include <stdio.h>
+#include <stdlib.h>
+
+int values[] = { 40, 10, 100, 90, 20, 25 };
+
+int compare (const void * a, const void * b)
+{
+  return ( *(int*)a - *(int*)b );
+}
+
+int main ()
+{
+  int n;
+  qsort (values, 6, sizeof(int), compare);
+  for (n=0; n<6; n++)
+     printf ("%d ",values[n]);
+  return 0;
+}
+
+
+Output:
+
+10 20 25 40 90 100
+
+**********
+See also
+bsearch
+Binary search in array (function )
