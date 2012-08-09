9d81d3473d383e95a379c3c6cb4af4d7e89d24c0
Guangwei Jiang <guangwei_jiang@wistron.com>
2012-08-09 10:24:16 +0800

    first commit IntSetSTL in ch13.
    Owner: Guangwei Jiang
    
    Issue: N/A
    
    Description: first commit IntSetSTL in ch13.
    
    Root Cause: N/A

---
+++ ch13_searching/IntSetSTL/IntSetSTL.cpp
@@ -0,0 +1,83 @@
+/*
+file name: IntSetSTL.cpp
+purpose: 
+	次算法利用C++标准模板库，用std::set表示集合。
+creator: guangwei jiang
+create time: 2012-08-09
+
+modify history:
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <iostream>
+#include <set>
+ 
+using namespace std;
+
+// notes: bigrand should at least 64bits
+unsigned long long bigrand()
+{
+	return RAND_MAX*rand() + rand();
+}
+
+class IntSetSTL {	
+private:
+	set<int> S;
+public:
+	IntSetSTL (int maxelements, int maxval){}
+	int size() {return S.size();}
+	void insert (int t){S.insert(t);}
+	void report(int *v)
+	{
+		int j = 0;
+		set<int>::iterator i;
+		for (i = S.begin(); i != S.end(); ++i)
+			v[j++] = *i;
+	}
+};
+
+/*
+ * parameter: 
+ *		m - the max number of elements
+ *		n - the max value
+ */
+void gensets (int m, int n)
+{
+	int *v = new int[m];
+	IntSetSTL S(m, n);
+
+	srand((int)time(0));
+
+	while (S.size() < m)
+		  S.insert(bigrand()%n);
+	
+	S.report(v);
+	for (int i = 0; i < m; i++)
+	{
+		cout << v[i] << "\t";
+		if ((i+1)%10 == 0)
+			cout << "\n";
+	}
+}
+
+int main()
+{
+	clock_t start, finish;
+	double duration;
+	int m, n;
+
+	m = 50000;
+	n = 10000000;
+
+	start = clock();
+	gensets(m, n);
+	finish = clock();
+
+	duration = (double)(finish - start) / CLOCKS_PER_SEC;
+	cout << "\n" << "m is "<< m << "; n is " << n << "\n";
+	cout << "Cost time: " << duration << " seconds \n";
+
+	return 0;
+}
